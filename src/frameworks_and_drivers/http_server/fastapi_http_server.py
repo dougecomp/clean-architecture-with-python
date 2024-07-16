a84af585cc147c49477a05f274f743c0622faaef
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 10:13:52 -0300

    feat: encaminhar payload caso exista em requisições POST para o controller

--- src/frameworks_and_drivers/http_server/fastapi_http_server.py
+++ src/frameworks_and_drivers/http_server/fastapi_http_server.py
@@ -8,15 +8,17 @@ class FastApiHttpServer(HttpServer):
     def __init__(self):
         self.app = FastAPI()
 
-    def __handle(self, controller: HttpController, request: Request, response: Response):
-        input = request.path_params | request.query_params._dict
+    async def __handle(self, controller: HttpController, request: Request, response: Response):
+        json = await request.json() if request.method == "POST" else {}
+        input = request.path_params | request.query_params._dict | json
+        print(input)
         controllerOutput = controller.handle(input)
         response.status_code = controllerOutput.status_code
         return controllerOutput.body
 
     def register(self, route: str, method: str, controller: HttpController):
-        def handler(request: Request, response: Response):
-            return self.__handle(controller=controller, request=request, response=response)
+        async def handler(request: Request, response: Response):
+            return await self.__handle(controller=controller, request=request, response=response)
 
         self.app.add_api_route(
             path=route,
