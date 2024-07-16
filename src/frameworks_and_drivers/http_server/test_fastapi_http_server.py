b7d955a83b806b907b48bad19a818105380c5a0d
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 09:20:14 -0300

    tests: garantir captura dos query_params na request

--- src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
+++ src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from fastapi.testclient import TestClient
 
 from src.interface_adapters.contracts.http_controller import HttpController
@@ -25,3 +27,14 @@ def test_return_controller_response_when_request_is_handled():
 
   assert response.status_code == 200
   assert response.json() == 'Hello World!'
+
+def test_can_forward_query_params_to_controller():
+  controllerMock = Mock(spec=HttpController)
+  controllerMock.handle.return_value = HttpResponse(status_code=200, body='')
+  api_server = FastApiHttpServer()
+  api_server.register(method="GET", route="/any_route", controller=controllerMock)
+  client = TestClient(api_server.app)
+
+  client.get(url="/any_route?name=any_name")
+
+  controllerMock.handle.assert_called_once_with({"name": "any_name"})
