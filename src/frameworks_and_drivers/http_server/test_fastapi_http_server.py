51f6ff9e54a921c831d979ed044c64bb57f0a07d
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 11:30:36 -0300

    tests: utilizar fixture para criar as dependências e reutilizar nos testes

--- src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
+++ src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
@@ -1,3 +1,4 @@
+from typing import TypedDict
 import pytest
 from unittest.mock import Mock
 
@@ -5,58 +6,65 @@ from fastapi.testclient import TestClient
 
 from src.interface_adapters.contracts.http_controller import HttpController
 from src.interface_adapters.contracts.http_response import HttpResponse
+from src.interface_adapters.contracts.http_server import HttpServer
 from .fastapi_http_server import FastApiHttpServer
 
-class ControllerSpy (HttpController):
-  def handle(self, http_request):
-    return HttpResponse(status_code=200, body='Hello World!')
-  
-def test_return_not_found_when_route_not_found():
+class SetUpType (TypedDict):
+  controller: Mock
+  api_server: HttpServer
+  client: TestClient
+
+@pytest.fixture
+def set_up() -> SetUpType:
+  controller_mock = Mock(HttpController)
+  controller_mock.handle.return_value = HttpResponse(status_code=200, body='Hello World!')
   api_server = FastApiHttpServer()
   client = TestClient(api_server.app)
+  return {'controller': controller_mock, 'api_server': api_server, 'client': client}
+
+def test_return_not_found_when_route_not_found(set_up):
+  client = set_up['client']
 
   response = client.get("/any_route")
 
   assert response.status_code == 404
 
-def test_return_controller_response_when_request_is_handled():
-  api_server = FastApiHttpServer()
-  api_server.register(method="GET", route="/any_route", controller=ControllerSpy())
-  client = TestClient(api_server.app)
+def test_return_controller_response_when_request_is_handled(set_up):
+  controllerMock = set_up['controller']
+  api_server = set_up['api_server']
+  client = set_up['client']
+  api_server.register(method="GET", route="/any_route", controller=controllerMock)
 
   response = client.get(url="/any_route")
 
   assert response.status_code == 200
   assert response.json() == 'Hello World!'
 
-def test_can_forward_query_params_to_controller():
-  controllerMock = Mock(spec=HttpController)
-  controllerMock.handle.return_value = HttpResponse(status_code=200, body='')
-  api_server = FastApiHttpServer()
+def test_can_forward_query_params_to_controller(set_up):
+  controllerMock = set_up['controller']
+  api_server = set_up['api_server']
+  client = set_up['client']
   api_server.register(method="GET", route="/any_route", controller=controllerMock)
-  client = TestClient(api_server.app)
 
   client.get(url="/any_route?name=any_name")
 
   controllerMock.handle.assert_called_once_with({"name": "any_name"})
 
-def test_can_forward_path_params_to_controller():
-  controllerMock = Mock(spec=HttpController)
-  controllerMock.handle.return_value = HttpResponse(status_code=200, body='')
-  api_server = FastApiHttpServer()
+def test_can_forward_path_params_to_controller(set_up):
+  controllerMock = set_up['controller']
+  api_server = set_up['api_server']
+  client = set_up['client']
   api_server.register(method="GET", route="/any_route/{name}", controller=controllerMock)
-  client = TestClient(api_server.app)
 
   client.get(url="/any_route/any_name")
 
   controllerMock.handle.assert_called_once_with({"name": "any_name"})
 
-def test_can_forward_body_params_to_controller():
-  controllerMock = Mock(spec=HttpController)
-  controllerMock.handle.return_value = HttpResponse(status_code=200, body='')
-  api_server = FastApiHttpServer()
+def test_can_forward_body_params_to_controller(set_up):
+  controllerMock = set_up['controller']
+  api_server = set_up['api_server']
+  client = set_up['client']
   api_server.register(method="POST", route="/any_route", controller=controllerMock)
-  client = TestClient(api_server.app)
 
   client.post(url="/any_route", json={"name": "any_name"})
 
