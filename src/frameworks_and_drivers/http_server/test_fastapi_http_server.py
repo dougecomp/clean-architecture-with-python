c4c8349194a0a934510992f081fa4a6084b71057
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 12:05:12 -0300

    tests: alterar para controller_mock

--- src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
+++ src/frameworks_and_drivers/http_server/test_fastapi_http_server.py
@@ -31,10 +31,10 @@ def test_return_not_found_when_route_not_found(set_up):
   assert response.status_code == 404
 
 def test_return_controller_response_when_request_is_handled(set_up):
-  controllerMock = set_up['controller']
+  controller_mock = set_up['controller']
   api_server = set_up['api_server']
   client = set_up['client']
-  api_server.register(method="GET", route="/any_route", controller=controllerMock)
+  api_server.register(method="GET", route="/any_route", controller=controller_mock)
 
   response = client.get(url="/any_route")
 
@@ -42,31 +42,31 @@ def test_return_controller_response_when_request_is_handled(set_up):
   assert response.json() == 'Hello World!'
 
 def test_can_forward_query_params_to_controller(set_up):
-  controllerMock = set_up['controller']
+  controller_mock = set_up['controller']
   api_server = set_up['api_server']
   client = set_up['client']
-  api_server.register(method="GET", route="/any_route", controller=controllerMock)
+  api_server.register(method="GET", route="/any_route", controller=controller_mock)
 
   client.get(url="/any_route?name=any_name")
 
-  controllerMock.handle.assert_called_once_with({"name": "any_name"})
+  controller_mock.handle.assert_called_once_with({"name": "any_name"})
 
 def test_can_forward_path_params_to_controller(set_up):
-  controllerMock = set_up['controller']
+  controller_mock = set_up['controller']
   api_server = set_up['api_server']
   client = set_up['client']
-  api_server.register(method="GET", route="/any_route/{name}", controller=controllerMock)
+  api_server.register(method="GET", route="/any_route/{name}", controller=controller_mock)
 
   client.get(url="/any_route/any_name")
 
-  controllerMock.handle.assert_called_once_with({"name": "any_name"})
+  controller_mock.handle.assert_called_once_with({"name": "any_name"})
 
 def test_can_forward_body_params_to_controller(set_up):
-  controllerMock = set_up['controller']
+  controller_mock = set_up['controller']
   api_server = set_up['api_server']
   client = set_up['client']
-  api_server.register(method="POST", route="/any_route", controller=controllerMock)
+  api_server.register(method="POST", route="/any_route", controller=controller_mock)
 
   client.post(url="/any_route", json={"name": "any_name"})
 
-  controllerMock.handle.assert_called_once_with({"name": "any_name"})
+  controller_mock.handle.assert_called_once_with({"name": "any_name"})
