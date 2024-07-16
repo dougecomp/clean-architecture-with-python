bcbc040eb22e854c1bbf1973b3839b4c0a6dc419
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 08:39:48 -0300

    refactor: definir handle como privado e explicitar como controllerOutput

--- src/frameworks_and_drivers/http_server/fastapi_http_server.py
+++ src/frameworks_and_drivers/http_server/fastapi_http_server.py
@@ -8,15 +8,15 @@ class FastApiHttpServer(HttpServer):
     def __init__(self):
         self.app = FastAPI()
 
-    def handle(self, controller: HttpController, request: Request, response: Response):
+    def __handle(self, controller: HttpController, request: Request, response: Response):
         input = request.path_params
-        output = controller.handle(input)
-        response.status_code = output.status_code
-        return output.body
+        controllerOutput = controller.handle(input)
+        response.status_code = controllerOutput.status_code
+        return controllerOutput.body
 
     def register(self, route: str, method: str, controller: HttpController):
         def handler(request: Request, response: Response):
-            return self.handle(controller, request, response)
+            return self.__handle(controller=controller, request=request, response=response)
 
         self.app.add_api_route(
             path=route,
