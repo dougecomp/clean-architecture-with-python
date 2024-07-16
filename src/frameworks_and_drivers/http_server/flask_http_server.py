d80ef7eb72dbe2a54d00be20f47024066e11edc7
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 20:36:58 -0300

    refactor: reduzir espaçamentos

--- src/frameworks_and_drivers/http_server/flask_http_server.py
+++ src/frameworks_and_drivers/http_server/flask_http_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, jsonify, Response
 
 from src.interface_adapters.contracts.http_controller import HttpController
 from src.interface_adapters.contracts.http_server import HttpServer
@@ -13,20 +13,25 @@ class FlaskHttpServer(HttpServer):
 
   def __handle(
         self, controller: HttpController
-    ):
-        json = request.get_json() if request.is_json else {}
-        input = request.values.to_dict() | json
-        controller_output = controller.handle(input)
-        response = jsonify(controller_output.body)
-        response.status_code = controller_output.status_code
-        response.default_mimetype = "application/json"
-        return response
+    ) -> Response:
+      json = request.get_json() if request.is_json else {}
+      query_params = request.args.to_dict()
+      path_params = {}
+      input = query_params | path_params | json
+      controller_output = controller.handle(input)
+      response = jsonify(controller_output.body)
+      response.status_code = controller_output.status_code
+      response.default_mimetype = "application/json"
+      return response
 
   def register(self, route: str, method: str, controller: HttpController):
     def handler():
       return self.__handle(
-          controller=controller
+        controller=controller
       )
-      
-    self.app.add_url_rule(rule=route, endpoint=route, methods=[method], view_func=handler)
-    pass
+
+    self.app.add_url_rule(
+      route,
+      methods=[method],
+      view_func=handler,
+    )
