18d5123ff6dadb2fc3f872af6f3818ea2b907b4c
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 20:38:54 -0300

    tests: garantir encaminhamento do body para o controller

--- src/frameworks_and_drivers/http_server/test_flask_http_server.py
+++ src/frameworks_and_drivers/http_server/test_flask_http_server.py
@@ -37,3 +37,15 @@ def test_can_forward_query_params_to_controller():
     client.get("/any_route?name=any_name")
 
     controller_mock.handle.assert_called_once_with({"name": "any_name"})
+  
+def test_can_forward_body_params_to_controller():
+    http_server = FlaskHttpServer()
+    client = http_server.app.test_client()
+
+    controller_mock = Mock()
+    controller_mock.handle.return_value = default_controller_response
+    http_server.register(route="/any_route", method="POST", controller=controller_mock)
+
+    client.post("/any_route", json={"name": "any_name"})
+
+    controller_mock.handle.assert_called_once_with({"name": "any_name"})
