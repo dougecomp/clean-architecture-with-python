b4642c858a4c3987e2175bd4ccfed4d1212686bf
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 17:21:58 -0300

    tests: garantir encaminhamento dos query_params para o controller

--- src/frameworks_and_drivers/http_server/test_flask_http_server.py
+++ src/frameworks_and_drivers/http_server/test_flask_http_server.py
@@ -21,7 +21,19 @@ def test_return_controller_response_when_request_is_handled():
     controller_mock.handle.return_value = default_controller_response
     http_server.register(route="/any_route", method="GET", controller=controller_mock)
 
-    response = client.get("/any_route", json={})
+    response = client.get("/any_route")
 
     assert response.status_code == default_controller_response.status_code
     assert response.get_json() == default_controller_response.body
+
+def test_can_forward_query_params_to_controller():
+    http_server = FlaskHttpServer()
+    client = http_server.app.test_client()
+
+    controller_mock = Mock()
+    controller_mock.handle.return_value = default_controller_response
+    http_server.register(route="/any_route", method="GET", controller=controller_mock)
+
+    client.get("/any_route?name=any_name")
+
+    controller_mock.handle.assert_called_once_with({"name": "any_name"})
