d29933b19dd9b6d1e74712559d4505de623a9d8e
Douglas Silva <douglaseusilva@gmail.com>
2024-07-16 23:53:20 -0300

    feat: implementação de servidor http através do AIOHTTP

---
+++ src/frameworks_and_drivers/http_server/aiohttp_http_server.py
@@ -0,0 +1,29 @@
+from aiohttp import web
+
+from src.interface_adapters.contracts.http_controller import HttpController
+from src.interface_adapters.contracts.http_server import HttpServer
+
+
+class AIOHttpHttpServer(HttpServer):
+  app: web.Application
+  def __init__(self):
+    self.app = web.Application()
+
+  async def __handle(
+      self, controller: HttpController, request: web.Request
+  ) -> web.Response:
+    json = await request.json() if request.method == "POST" else {}
+    query_params = request.query.__dict__
+    path_params = {}
+    input = query_params | path_params | json
+    controller_output = controller.handle(input)
+    return web.Response(body=controller_output.body, status=controller_output.status_code)
+
+  def register(self, route: str, method: str, controller: HttpController):
+    async def handler(request: web.Request):
+        return await self.__handle(
+            controller=controller,
+            request=request
+        )
+
+    self.app.router.add_route(method, route, handler)
